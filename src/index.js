46dceaa8c994d90e2443da8df564a7d661a6dfe9
myucch <teresa.ye@yahoo.com>
2022-02-06 20:57:30 +0100

    Added conversion links of Celsius and Fahrenheit in JS

--- src/index.js
+++ src/index.js
@@ -25,6 +25,52 @@ if (minutes < 10) {
 
 weatherDate.innerHTML = `${day} ${hours}:${minutes}`;
 
+// Retrieve location
+function retrievePosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showTemperature);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let button = document.querySelector("#current-location-button");
+button.addEventListener("click", getCurrentPosition);
+
+// Fahrenheit and Celsius converter
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-main");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-main");
+
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusTemp = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
 function showTemperature(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector("#temperature-main").innerHTML = Math.round(
@@ -42,16 +88,18 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  celsiusTemp = response.data.main.temp;
 }
 
-// API integration search city in box
+// API integration search city
 function searchCity(city) {
   let apiKey = "8c83aaa11682fdeb36d3e77599574e7c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showTemperature);
 }
 
-// Submit city event *trigger*
 function submitCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input").value;
@@ -61,17 +109,4 @@ function submitCity(event) {
 let form = document.querySelector("#search-city-form");
 form.addEventListener("submit", submitCity);
 
-function retrievePosition(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(showTemperature);
-}
-
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(retrievePosition);
-}
-
-let button = document.querySelector("#current-location-button");
-button.addEventListener("click", getCurrentPosition);
+searchCity("Leuven");
